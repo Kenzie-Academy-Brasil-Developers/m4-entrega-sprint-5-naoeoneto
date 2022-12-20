9b56901778d7ed2aeae28ea9dac3e8e8a4c91a11
naoeoneto <abdpneto@gmail.com>
2022-12-20 14:41:32 -0300

    initial commit

---
+++ src/entities/property.entity.ts
@@ -0,0 +1,27 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Address } from "./address.entity";
+
+@Entity('properties')
+export class Propoerty {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column({ default: false })
+    sold: boolean
+
+    @Column()
+    value: number
+
+    @Column()
+    size: number
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+
+    @OneToOne (() => Address)
+    @JoinColumn()
+    address: Address
+}
